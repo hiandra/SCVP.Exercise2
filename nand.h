2fb6533af64bff5e05e722f1ea5076cf3c890f77
Hiandra Tomasi <hiandra_tomasi@hotmail.com>
2021-12-12 02:50:46 +0100

    [Task 1]
       - Implemented solution for Task1.

--- nand.h
+++ nand.h
@@ -6,4 +6,23 @@
 
 // TODO: Insert your code here
 
+SC_MODULE(nand)
+{
+    sc_in<bool> A;
+    sc_in<bool> B;
+    sc_out<bool> Z;
+
+    SC_CTOR(nand) : A("A"), B("B"), Z("Z")
+    {
+        SC_METHOD(do_nand);
+        sensitive << A << B;
+    }
+
+    void do_nand()
+    {
+        Z.write( !( A.read() && B.read() ) );
+        std::cout << "Executing do_nand:" << " simulation time " << sc_time_stamp().to_default_time_units() << " ps Δ cycle " << sc_delta_count() << ":\tA " << (A.read() ? "'1'" : "'0'") << " B " << (B.read() ? "'1'" : "'0'") << " Z " << (Z.read() ? "'1'" : "'0'") << std::endl;
+    }
+};
+
 #endif
